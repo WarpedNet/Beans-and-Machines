6da89238fbf1b2c9d0a3c57aa80aa1b16f9ef28c
Jaye <119345329+NotABusDriver@users.noreply.github.com>
2024-03-06 12:28:12 +0000

    Updated ERD
    Removed Login and Cart tables from the ERD.

--- lib/databaseConf.php
+++ lib/databaseConf.php
@@ -1,8 +1,8 @@
 <?php
 
 $databaseVars = array(
-	"ip" => "mysql:dbname=beansdb;host=localhost",
-	"username" => "root",
+	"ip" => "mysql:dbname=beansdb;host=10.9.98.190",
+	"username" => "Jake",
 	"password" => null,
 );
 
