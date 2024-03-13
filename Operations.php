8bbdbfc89f5769a5098aa7614b4d1195b2134b51
CJBreen <B00152224@mytudublin.ie>
2024-03-13 12:12:20 +0000

    started crud

---
+++ Operations.php
@@ -0,0 +1,29 @@
+<?php
+
+class Operations
+{
+    function getConnection()
+    {
+        $databaseConf = require "databaseConf.php";
+
+        $pdo = new PDO(
+            $databaseConf["ip"],
+            $databaseConf["username"],
+            $databaseConf["password"]
+        );
+        return $pdo;
+    }
+    private function read($column, $table)
+    {
+        $pdo =  $this->getConnection();
+        $query = "SELECT ? FROM ?";
+        $stmt = $pdo ->prepare($query);
+        $stmt->bindParam(1,$column, PDO::PARAM_STR);
+        $stmt->bindParam(2,$table,PDO::PARAM_STR);
+        $stmt->execute();
+        $data = $stmt->fetchAll();
+        return $data;
+        
+    }
+
+}
