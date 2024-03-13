2ca45237eb3d329063fb08be3cd44d8261e7613b
WarpedNet <B00152862@mytudublin.ie>
2024-03-13 10:33:12 +0000

    Classes moved

--- class/user/Customer.php
+++ class/user/Customer.php
@@ -6,33 +6,23 @@ class Customer
 {
     private $isMember;
     private $address;
-    /**
-     * @return mixed
-     */
     public function getIsMember()
     {
         return $this->isMember;
     }
 
-    /**
-     * @param mixed $isMember
-     */
+    
     public function setIsMember($isMember)
     {
         $this->isMember = $isMember;
     }
 
-    /**
-     * @return mixed
-     */
     public function getAddress()
     {
         return $this->address;
     }
 
-    /**
-     * @param mixed $address
-     */
+    
     public function setAddress($address)
     {
         $this->address = $address;
