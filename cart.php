9a22e3f4d738e259607b60267a8f09549d12534d
WarpedNet <102426484+WarpedNet@users.noreply.github.com>
2024-03-19 02:14:47 +0000

    Update Cart

--- cart.php
+++ cart.php
@@ -1,53 +1,36 @@
-<!-- including css -->
-<link rel="stylesheet" type="text/css" href="CSS/index.css">
-
-<!-- require header and database related files -->
 <?php
-	require 'layout/header.php'; 
-	require_once 'lib/Operations.php';
-	require 'lib/CRUD Operations/ReadProducts.php';
-	require 'class/product/Product.php';
-	require 'class/product/Cart.php';
-	use class\product\Product as productObj;
-	use class\product\Cart as cartObj;
-
-	// creating an array of products
-	$readProductsObj = new readProducts();
-	$productObjArr = array();
-	$cartObj = new cartObj();
-
-	// referencing the readData function
-	$productData = $readProductsObj->readData("*");
-
-	// displaying the data
-	foreach ($productData as $product) {
-		$productObj = new productObj;
-		$productObj->setProductID($product[0]);
-		$productObj->setProductName($product[1]);
-		$productObj->setExpiraryDate($product[2]);
-		$productObj->setStock($product[3]);
-		$productObjArr[] = $productObj;
-	}
+session_start();
+if (!$_SESSION['Active']){
+    header("Location: login.php");
+    // exit;
+}
 ?>
 
-<!-- table for displaying information -->
+<?php 
+// require 'class/product/Cart.php';
+// use class\product\Cart as cartObj;
+
+if (isset($_SESSION['Cart'])) {
+	$cartObj = $_SESSION['Cart'];
+}
+
+
+
+?>
+<link rel="stylesheet" type="text/css" href="CSS/index.css">
+<?php require 'layout/header.php'?>
 <div class="main-content" align="center">
-	<h1 style="font-family:sans-serif;">Shopping-Cart</h1>
-		<table>
+	<h1 style="font-family:sans-serif;">Cart</h1>
+	<table>
 		<tr>
 			<th>Product ID</th>
 			<th>Product</th>
 			<th>Expirary Date</th>
 			<th>Stock</th>
 		</tr>
-		<?php foreach ($productObjArr as $product) {?>
-			<tr>
-				<?php $product->displayProduct();?>
-				<td>
-			</tr>
-		<?php } ?>
+		<?php $cartObj->displayCart() ?>
+
 	</table>
 </div>
 
-<!-- setting the footer at the bottom of the page -->
 <?php require 'layout/footer.php'; ?>
