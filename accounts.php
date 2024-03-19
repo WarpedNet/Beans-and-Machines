309a52da6e18f5ab84195d7230ba9811dcd24e44
WarpedNet <102426484+WarpedNet@users.noreply.github.com>
2024-03-19 04:14:29 +0000

    tried fixing accounts.php

--- accounts.php
+++ accounts.php
@@ -1,30 +1,40 @@
 <?php
 session_start();
+require "lib/CRUD Operations/readUserData.php";
 //usernames & passwords? IDK I'll just use this as a test
 //this will be used for sending account info to the database
 //thank you to "login with sessions v4.pdf" for teaching me this
-$user = $_POST['userInput'];
-$pass = $_POST['passInput'];
+if (isset($_POST['userInput']) && isset($_POST['passInput'])) {
+    $user = $_POST['userInput'];
+    $pass = $_POST['passInput'];
 
-//creating a query to check if the username + password is in the database
-$userquery = "SELECT * FROM logininfo where lPassword = '$pass' and lEmail = '$user'";
-$adminquery = "SELECT * FROM admin where isAdmin = 'true'";
-// this is a statement wow
-$stmt = exec($userquery) && exec($adminquery) or die();
-
-
-if (isset($_POST['submit'])) {
-    if (($_SESSION['userInput'] == $user) && ($_SESSION['passInput'] == $pass) && (!$adminquery)) {
-        $_SESSION['Username'] = $user;
-        $_SESSION['Active'] = true;
-        header("Location: index.php");
-        exit;
+    //creating a query to check if the username + password is in the database
+    $userquery = "SELECT * FROM users where uPassword = '$pass' and uEmail = '$user'";
+    // $adminquery = "SELECT * FROM admin where isAdmin = 'true'";
+    // this is a statement wow
+    $readUserDataObj = new readUserData();
+    $data = $readUserDataObj->displayUserData($userquery)[0];
+    if (strlen($data[2])>0) {
+        $user = true;
     }
-    elseif (($_SESSION['userInput'] == $user) && ($_SESSION['passInput'] == $pass) && ($adminquery)) {
-        $_SESSION['Username'] =  $user;
-        $_SESSION['Active'] = true;
-        //header("Location: admin.php or whereever")
-        exit;
-        
+    if (strlen($data[3]>0)) {
+        $pass = true;
+    }
+
+
+    if (isset($_POST['submit'])) {
+        if (($_SESSION['userInput'] == $user) && ($_SESSION['passInput'] == $pass) && (!$adminquery)) {
+            $_SESSION['Username'] = $user;
+            $_SESSION['Active'] = true;
+            header("Location: index.php");
+            exit;
+        }
+        elseif (($_SESSION['userInput'] == $user) && ($_SESSION['passInput'] == $pass) && ($adminquery)) {
+            $_SESSION['Username'] =  $user;
+            $_SESSION['Active'] = true;
+            //header("Location: admin.php or whereever")
+            exit;
+            
+        }
     }
 }
--- lib/CRUD Operations/readUserData.php
+++ lib/CRUD Operations/readUserData.php
@@ -4,11 +4,10 @@ require "DatabaseConnection.php";
 
 class readUserData extends databaseCon
 {
-    function displayUserData($column): bool|array
+    function displayUserData($query): bool|array
     {
         $displayOBJ = new databaseCon();
         $pdo = $displayOBJ->getConnection();
-        $query = 'SELECT * FROM users';
         $stmt = $pdo->prepare($query);
         $stmt->execute();
         return $stmt->fetchAll();
