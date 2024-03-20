3270b47cc515acef593274b6fd523d987f6b5778
WarpedNet <B00152862@mytudublin.ie>
2024-03-20 09:30:20 +0000

    login fixed?

--- accounts.php
+++ accounts.php
@@ -4,37 +4,38 @@ require "lib/CRUD Operations/readUserData.php";
 //usernames & passwords? IDK I'll just use this as a test
 //this will be used for sending account info to the database
 //thank you to "login with sessions v4.pdf" for teaching me this
-if (isset($_POST['userInput']) && isset($_POST['passInput'])) {
-    $user = $_POST['userInput'];
-    $pass = $_POST['passInput'];
+$user = "callum";
+$pass = "password";
+// if (isset($_POST['userInput']) && isset($_POST['passInput'])) {
+//     $user = $_POST['userInput'];
+//     $pass = $_POST['passInput'];
 
-    //creating a query to check if the username + password is in the database
-    $userquery = "SELECT * FROM users where uPassword = '$pass' and uEmail = '$user'";
-    // $adminquery = "SELECT * FROM admin where isAdmin = 'true'";
-    // this is a statement wow
-    $readUserDataObj = new readUserData();
-    $data = $readUserDataObj->displayUserData($userquery)[0];
-    if (strlen($data[2])>0) {
-        $user = true;
+//     //creating a query to check if the username + password is in the database
+//     $userquery = "SELECT * FROM users where uPassword = '$pass' and uEmail = '$user'";
+//     // $adminquery = "SELECT * FROM admin where isAdmin = 'true'";
+//     // this is a statement wow
+//     $readUserDataObj = new readUserData();
+//     $data = $readUserDataObj->displayUserData($userquery)[0];
+//     if (strlen($data[2])>0) {
+//         $user = true;
+//     }
+//     if (strlen($data[3]>0)) {
+//         $pass = true;
+//     }
+
+
+if (isset($_POST['submit'])) {
+    if (($_POST['userInput'] == $user) && ($_POST['passInput'] == $pass)) {
+        $_SESSION['Username'] = $user;
+        $_SESSION['Active'] = true;
+        header("Location: index.php");
+        exit;
     }
-    if (strlen($data[3]>0)) {
-        $pass = true;
-    }
-
-
-    if (isset($_POST['submit'])) {
-        if (($_SESSION['userInput'] == $user) && ($_SESSION['passInput'] == $pass) && (!$adminquery)) {
-            $_SESSION['Username'] = $user;
-            $_SESSION['Active'] = true;
-            header("Location: index.php");
-            exit;
-        }
-        elseif (($_SESSION['userInput'] == $user) && ($_SESSION['passInput'] == $pass) && ($adminquery)) {
-            $_SESSION['Username'] =  $user;
-            $_SESSION['Active'] = true;
-            //header("Location: admin.php or whereever")
-            exit;
-            
-        }
+    elseif (($_POST['userInput'] == $user) && ($_POST['passInput'] == $pass)) {
+        $_SESSION['Username'] =  $user;
+        $_SESSION['Active'] = true;
+        //header("Location: admin.php or whereever")
+        exit;
+        
     }
 }
