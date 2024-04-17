c2554f02b58596a605913868f05514b8e4a0c306
WarpedNet <B00152862@mytudublin.ie>
2024-04-17 09:48:55 +0100

    Config
    Callum deleted it

--- src/config.php
+++ src/config.php
@@ -1,1 +1,11 @@
 <?php
+
+	$host = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "beansdb";
+	$dsn = "mysql:host=$host;dbname=$dbname";
+	$options = array(
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+	);
+?>
