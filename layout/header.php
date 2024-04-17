2cd640edb182cba4effaed997da7039c4b67a54f
WarpedNet <B00152862@mytudublin.ie>
2024-04-17 10:23:31 +0100

    Fixed mistakes

--- layout/header.php
+++ layout/header.php
@@ -1,5 +1,8 @@
 <?php
-    session_start(); /* Starts the session */
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+} 
+/* Starts the session */
 if($_SESSION['Active'] == false){ /* Redirects user to login.php if not logged in */
     header("location:login.php");
     exit;
--- src/DBconnect.php
+++ src/DBconnect.php
@@ -3,7 +3,6 @@ require_once 'config.php'; //access the login values
 
 try {
     $connection = new PDO($dsn, $username, $password);
-    echo 'DB connected';
 } catch (\PDOException $e) {
     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
