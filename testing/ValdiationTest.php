c03e2f3e44508340e8b8007a2762109ddee3ab7a
WarpedNet <102426484+WarpedNet@users.noreply.github.com>
2024-04-28 14:56:53 +0100

    validation test

--- testing/ValdiationTest.php
+++ testing/ValdiationTest.php
@@ -2,7 +2,26 @@
 
 require_once "../src/validation.php";
 
-$valid = array(
-	""
+$validValues = array(
+	"Password1#",
+	"321StreetSesame(",
+	"555555P#",
+	"Bottle5*",
+	"£sampleStreet5"
 );
+
+$invalidValues = array(
+	"password",
+	"password1",
+	"1",
+	"",
+	"182765",
+	"A#123456789101112131415161718192021222324252627282930"
+);
+foreach ($validValues as $value) {
+	echo nl2br("Input: ".$value."\n"."Result: ".passwordVerify($value)."\n\n");
+}
+foreach ($invalidValues as $value) {
+	echo nl2br("Input: ".$value."\n"."Result: ".passwordVerify($value)."\n\n");
+}
 ?>
