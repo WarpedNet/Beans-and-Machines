94d28c1802b3c79a6451b6957200d5ab34d42490
Jaye <119345329+NotABusDriver@users.noreply.github.com>
2024-04-28 16:24:33 +0100

    Basis path testing

--- testing/BPathTest.php
+++ testing/BPathTest.php
@@ -1,3 +1,19 @@
-<?php
+<?php 
+	// this will be the sample passwords for our basis path testing
+	$pathOne = "Password123!";
+	$pathTwo = "password123!";
+	$pathThree = "Password123";
+	$pathFour = "";
+	$pathFive = "Password!";
+	$pathSix = "Pw1!";
 
-//
+	// method to check if password is valid for each basis path test
+	 require_once '../src/validation.php';
+	 
+	 passwordVerify($pathOne); // pass
+	 passwordVerify($pathTwo); // fail
+	 passwordVerify($pathThree); // fail
+	 passwordVerify($pathFour); // fail
+	 passwordVerify($pathFive); // fail
+	 passwordVerify($pathSix); // fail
+ ?>
